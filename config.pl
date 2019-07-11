9c74e04e66bb64920828ccad164045c32854c789
Aswin van Woudenberg <reachme@aswinvanwoudenberg.com>
2019-07-11 17:45:02 +0200

    Update config.pl

--- config.pl
+++ config.pl
@@ -6,7 +6,7 @@ public_port(<<PUBLIC_PORT_OF_SERVER>>).
 server_port(<<PORT_OF_SERVER>>).
 
 ping('<<NABAZTAG_MAC_ADDRESS>>',10).
-ambient('<<NABAZTAG_MAC_ADDRESS>>',[(messages,3),(weather,storm)],0,0,0).
+ambient('<<NABAZTAG_MAC_ADDRESS>>',[],0,0,0).
 ifttt('<<NABAZTAG_MAC_ADDRESS>>','<<IFTTT_WEBHOOK_KEY>>').
 packets('<<NABAZTAG_MAC_ADDRESS>>',[]).
 
